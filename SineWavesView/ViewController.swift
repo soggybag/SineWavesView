543fad8cbc7528b1fafebbdc2535730435af7000
Mitchell Hudson <soggybag@gmail.com>
2016-03-05 21:53:22 -0800

    update

--- SineWavesView/ViewController.swift
+++ SineWavesView/ViewController.swift
@@ -18,9 +18,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let sineWave1 = SineWave(amplitude: 25, frequency: 1.8, rate: -0.003, color: UIColor(hue: 0, saturation: 1.0, brightness: 1.0, alpha: 0.5))
-        let sineWave2 = SineWave(amplitude: 18, frequency: 1.34, rate: -0.013, color: UIColor(hue: 0.33, saturation: 1.0, brightness: 1.0, alpha: 0.5))
-        let sineWave3 = SineWave(amplitude: 33, frequency: 1, rate: -0.011, color: UIColor(hue: 0.66, saturation: 1.0, brightness: 1.0, alpha: 0.5))
+        let sineWave1 = SineWave(amplitude: 25, frequency: 1.8, rate: -0.07, color:
+            UIColor(hue: 0.123, saturation: 1.0, brightness: 1.0, alpha: 0.5))
+        let sineWave2 = SineWave(amplitude: 18, frequency: 1.34, rate: -0.13, color:
+            UIColor(hue: 0.453, saturation: 1.0, brightness: 1.0, alpha: 0.5))
+        let sineWave3 = SineWave(amplitude: 33, frequency: 1, rate: -0.10, color:
+            UIColor(hue: 0.783, saturation: 1.0, brightness: 1.0, alpha: 0.5))
         
         sineWaveView.addSineWave(sineWave1)
         sineWaveView.addSineWave(sineWave2)
